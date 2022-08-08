311164c01339c7060ef6cb480746e4503b5a36b9
dimasinc <dimas_inc@mail.ru>
2022-08-09 01:15:22 +0300

    Итог

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 
+# Задача № 1:
 def intelligence_hero():
     id = input("Введите через запятую id героев для сравнения:").replace(' ', '')
     list_id = id.split(',')
@@ -16,9 +17,37 @@ def intelligence_hero():
                             intelligence_hero.update({'name': hero.json()['name'], 'intelligence': powerstats_dict['intelligence']})
     print(f"Герой с самым высоким интеллектом:{intelligence_hero['name']}, уровень интеллекта: {intelligence_hero['intelligence']}")
 
-intelligence_hero()
-
-
+# intelligence_hero()
 # Hulk id = 332
 # CA id = 149
 # Thanos id = 655
+
+
+# Задача № 2:
+Token = ' '
+class YandexDisk:
+    def __init__(self, token):
+        self.token = Token
+
+    def headers(self):
+        return {'Content-Type': 'application/json', 'Authorization': 'OAuth {}'.format(self.token)}
+
+    def upload_link(self, disk_file_path):
+        url = "http://cloud-api.yandex.net/v1/disk/resources/upload"
+        headers = self.headers()
+        params = {'path': disk_file_path, 'overwrite': 'true'}
+        response = requests.get(url, headers=headers, params=params)
+        return response.json()
+
+    def upload_disk(self, disk_file_path, filename):
+        href = self.upload_link(disk_file_path=disk_file_path).get('href', '')
+        response = requests.put(href, data=open(filename, 'rb'))
+
+
+
+
+file_up = YandexDisk(token=Token)
+
+# file_up.upload_disk(disk_file_path='netology/test.txt', filename='test.txt')
+
+
